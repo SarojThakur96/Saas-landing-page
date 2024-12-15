e10c2b4a3b8c31b10b3e3d8752279a0079804909
Saroj Thakur <sarojthakur247@gmail.com>
2024-12-15 22:34:01 +0530

    some animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,23 +1,74 @@
+"use client";
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import cylinderImage from "@/assets/cylinder.png";
 import Image from "next/image";
+import {
+  animate,
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import noodleImage from "@/assets/noodle.png";
+import { useEffect, useRef } from "react";
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  // useMotionValueEvent(scrollYProgress, "change", (latestValue) =>
+  //   console.log(latestValue)
+  // );
+  // const baseText = "Pathway to productivity " as string;
+  // const count = useMotionValue(0);
+  // const rounded = useTransform(count, (latest) => Math.round(latest));
+  // const displayText = useTransform(rounded, (latest) =>
+  //   baseText.slice(0, latest)
+  // );
+
+  // useEffect(() => {
+  //   const controls = animate(count, baseText.length, {
+  //     // type: "tween",
+  //     duration: 1,
+  //     ease: "easeInOut",
+  //   });
+  //   return controls.stop;
+  // }, []);
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip"
+    >
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
             <div className="tag">Version 2.0 is here</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              {/* {displayText} */}
               Pathway to productivity
             </h1>
-            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+            <motion.p
+              className="text-xl text-[#010D3E] tracking-tight mt-6"
+              animate={{
+                translateY: [20, 0],
+              }}
+              transition={{
+                // repeat: 1,
+                repeatType: "loop",
+                duration: 3,
+              }}
+            >
               Celebrate the joy of accomplishment with an app designed to track
               your progress, motivate your efforts, and celebrate your
               successes.
-            </p>
+            </motion.p>
             <div className="flex gap-1 items-center mt-[30px]">
               <button className="btn btn-primary">Get for free</button>
               <button className="btn btn-text gap-1">
@@ -27,23 +78,39 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="cogImage"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut",
+              }}
             />
-            <Image
-              src={cylinderImage}
+            <motion.img
+              src={cylinderImage.src}
               width={220}
               height={220}
               alt="cylinder image"
               className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{
+                translateY: translateY,
+              }}
             />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               width={220}
               alt="noodle image"
               className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg] "
+              style={{
+                rotate: 30,
+                translateY: translateY,
+              }}
             />
           </div>
         </div>
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,17 +1,27 @@
+"use client";
 import Image from "next/image";
 import acmeLogo from "@/assets/logo-acme.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
 import pulseLogo from "@/assets/logo-acme.png";
-import apexLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
-
+import { motion } from "framer-motion";
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{ translateX: "-50%" }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear",
+            }}
+          >
             <Image
               src={acmeLogo}
               alt="acheme logo"
@@ -43,7 +53,38 @@ export const LogoTicker = () => {
               alt="apex logo"
               className="logo-ticker-image"
             />
-          </div>
+            <Image
+              src={acmeLogo}
+              alt="acheme logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="quantum logo"
+              className="logo-ticker-image"
+            />
+
+            <Image
+              src={echoLogo}
+              alt="echo logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="celestial logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="pulse logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="apex logo"
+              className="logo-ticker-image"
+            />
+          </motion.div>
         </div>
       </div>
     </div>
